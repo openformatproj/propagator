492e244c667deb9ce7985dfd8e8159ba651100b7
TRIFOGLIOA <alessandro.trifoglio@leonardo.com>
2025-09-24 11:14:58 +0200

    Added __init__

---
+++ __init__.py
@@ -0,0 +1,32 @@
+"""
+Propagator: A lightweight, dependency-aware build and update engine for Python.
+"""
+
+from .engine import (
+    Propagator,
+    Resource,
+    Location,
+    FileLocation,
+    Event,
+    Error,
+    void_function,
+)
+
+from .types import (
+    EventTypes,
+    ErrorTypes,
+    PropagationLevel,
+)
+
+__all__ = [
+    "Propagator",
+    "Resource",
+    "Location",
+    "FileLocation",
+    "Event",
+    "Error",
+    "void_function",
+    "EventTypes",
+    "ErrorTypes",
+    "PropagationLevel",
+]
